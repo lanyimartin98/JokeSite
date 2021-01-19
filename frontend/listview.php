e4c58bfe810f4a6bb42469784422d036cc2be31b
gengeliczkip <74673354+gengeliczkip@users.noreply.github.com>
2021-01-19 18:55:15 +0100

    Update listview.php

--- frontend/listview.php
+++ frontend/listview.php
@@ -1,5 +1,13 @@
 <?php
 function generateListView($array){
 foreach ($array as $v) {
-    echo "";
+    echo "
+    <div class=\"joke\">
+    <div class=\"jokebody\">".
+    $v["full"]
+    ."</div></div>
+    </br>
+    ";
+  }
+}
 ?>
