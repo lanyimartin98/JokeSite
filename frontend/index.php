a0db6f89889f8e4bd4d323cc7f0d039d3df8bcfd
lanyimartin98 <55244702+lanyimartin98@users.noreply.github.com>
2021-01-19 18:41:24 +0100

    Index basics

---
+++ frontend/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<?php
+require_once('database.php');
+?>
+<html lang="hu">
+
+<head>
+	<meta charset="UTF-8">
+	
+	<title> Rettenetesen Fárasztó Tréfák</title>
+	<meta name="description" content="Rettenetesen Fárasztó Tréfák">
+	
+	<link rel="stylesheet" href="./assets/rftstyle.css" type="text/css">
+	
+<head>
