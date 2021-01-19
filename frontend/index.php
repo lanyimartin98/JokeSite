c1e609cee341ad83d474ba35c2bcfb542c1f81ea
lanyimartin98 <55244702+lanyimartin98@users.noreply.github.com>
2021-01-19 18:41:51 +0100

    Frontend basics

--- frontend/index.php
+++ frontend/index.php
@@ -13,3 +13,31 @@ require_once('database.php');
 	<link rel="stylesheet" href="./assets/rftstyle.css" type="text/css">
 	
 <head>
+<body>
+		
+	<h1 style="color:Black;"> Rettenetesen Fárasztó Tréfák </h1>
+	<p>Válassz egy tréfa témát!</p>
+	
+	<div class="container">
+		<div class="button-wrapper">
+			<a class="button button1" href="/jokesite/?c=2">Iskola</a>
+			<a class="button button2" href="/jokesite/?c=1">Rendőr</a>
+			<a class="button button3" href="/jokesite/?c=3">Sport</a>
+			<a class="button button4" href="/jokesite/?c=4">Állatos</a>
+			<a class="button button5" href="/jokesite/?c=5">Orvos</a>
+		</div>
+	</div>
+	
+	<hr>
+	<div class="container">
+	<div class="random">test</div>
+	<div>
+		<?php
+			require_once('routing.php');
+			?>
+	</div>
+	<div class="random">test</div>
+	</div>
+</body>
+
+</html>
