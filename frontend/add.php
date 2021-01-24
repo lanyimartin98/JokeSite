4ef3104b278a0adaa51df71e95f1db40db5a0470
lanyimartin98 <55244702+lanyimartin98@users.noreply.github.com>
2021-01-24 18:27:06 +0100

    Adding function

--- frontend/add.php
+++ frontend/add.php
@@ -1,4 +1,14 @@
 <form method="POST">
-<input type="text">
-<select></select>
+<table>
+<tr><td>Vicc szövege:</td></tr>
+<tr>
+<td><input type="text"></td>
+</tr>
+<tr><td>Vicc kategóriája:</td></tr>
+<tr>
+<tr>
+<td><select></select></td>
+</tr>
+<tr><td><button type = "submit" class = "submit" name = "login">Belépés</button></td></tr>
+</table>
 </form>
