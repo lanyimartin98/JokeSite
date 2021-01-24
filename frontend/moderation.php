f2f415ba3267c4ae05791cb02ef0d0c0addaa283
lanyimartin98 <55244702+lanyimartin98@users.noreply.github.com>
2021-01-24 19:02:51 +0100

    Accept decline system

--- frontend/moderation.php
+++ frontend/moderation.php
@@ -3,9 +3,13 @@ function generateModeration($array){
 foreach ($array as $v) {
     echo "
     <div class=\"joke\">
-    <div class=\"jokebody\">".
+    <table  class=\"jokebody\"><tr><td>".
     $v["full"]
-    ."</div></div>
+    ."</td>
+    </tr>
+    <tr><td><a href=/jokessite/JokeSite/route=agree&id=".$v['id'].">Elfogad</a></td><td><a href=/jokessite/JokeSite/route=decline&id=".$v['id'].">Elutasít</a></td></tr>
+    </table>
+    </div>
     </br>
     ";
 }
