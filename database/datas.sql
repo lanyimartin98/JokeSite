4ef8a8ee167d24d8a054e0f37764a9f7f5b537f4
gengeliczkip <74673354+gengeliczkip@users.noreply.github.com>
2021-01-24 18:24:44 +0100

    datas.sql updated

--- database/datas.sql
+++ database/datas.sql
@@ -26,3 +26,119 @@ VALUES
 (
 1, "Ki az abszolút optimista? </br>- ??? </br>- Az a férj, aki a feleségét a ruhabolt előtt járó motorral várja."
 );
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+1, "Ki az abszolút bátor? </br>- ??? </br>- Aki egyszerre veszi be az altatót és a hashajtót!"
+);
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+2, "Szóval azt állítja, hogy eltűnt az anyósa? </br>- Igen. </br>- Mikor? </br>- Úgy két évvel ezelőtt. </br>- Ember! Akkor miért csak most jelenti be? </br>- Valahogy eddig nem mertem elhinni."
+);
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+2, "Két férfi áll a temetőben egymás mellett. Az egyik megkérdi a másikat: </br>- Maga kit gyászol? </br>- Az anyósomat. Tegnapelőtt halt meg. </br>- Tényleg? Az enyém is. Micsoda szerencsés véletlen!"
+);
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+2, "Kovács úr és neje egy kertes házban lévő ismerősnél van látogatóban. A házigazda büszkén vezeti körbe vendégeit a gondozott kerten. Az egyik fánál megjegyzi: </br>- Ezt a fát akkor ültettem
+");
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+2, "Apu! Nem láttad a könyvemet a hosszú élet titkáról? </br>- Elégettem. </br>- Micsoda? Mégis, , hogy képzelted? </br>- Az anyósom nagyon olvasgatta mostanában…"
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+2, "Képzeld, a falióránk leesett a falról! Ha egy másodperccel hamarabb történik, pont az anyám fejére zuhan! </br>- Ez a fránya óra mindig is késett…"
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+2, "Amikor megnősültem,  azt mondtam az anyósomnak: Az én házam a te házad is. </br>- És? </br>- Másnap eladta."
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+3, "Belegondoltál már? Bárki, aki lassabban vezet nálad, idióta, míg bárki, aki gyorsabb nálad, az vadállat."
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+3, "Egy kispolski fékez egy Rolls Royce mellett a pirosnál. A vezetője átszól: </br>- Van rádió a kocsijában? </br>- Ez csak természetes! </br>- válaszol a Rolls Royce vezetője. </br>- És TV? </br>- Hát persze! </br>- És franciaágy? A Rolls Royce tulajdonosa elszégyelli magát, és elhajt a legközelebbi szervízbe, ahol egy franciaágyat szereltet a kocsijába. Egy hét múlva megpillantja a Polskit egy parkolóban. Odamegy, bekopog az ablakon, és elkezdi: </br>- Van már franciaágy a kocsimban! A kispolski vezetője rámordul: </br>- És ezért kellett megzavarnia zuhanyozás közben?"
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+3, "Hogyan duplázod meg a Trabant árát? </br>- ??? </br>- Teletankolod!"
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+3, " felháborodott autós az autóalkatrész boltban: </br>- Uram, Ön nekem azt mondta, hogy ez az akkumulátor nagyon tartós darab, és az autómba már soha többet nem kell vennem másikat! Aztán még hat hónapot sem bírt ki! </br>- Elnézést kérek, nem gondoltam volna, hogy az autója kibír még fél évet..."
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+3, "Járókelő kérdezi egy másiktól: </br>- Meg tudná nekem mondani, melyik a legrövidebb út a vasútállomásra? </br>- Nem tudom. </br>- Miért, nem idevalósi? </br>- De igen, csak én taxisofőr vagyok."
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+3, "Egy fickó meg egy rabbi ütköznek az autójukkal. Az autók totálkárosak, de ők megúszták egy karcolással. Kiszállnak az autóból és beszélgetni kezdenek. Egyszercsak megszólal a rabbi: </br>- Semmi baj, az a lényeg, hogy nekünk nem esett bajunk. Gyere testvérem, igyál velem egy kortyot! </br>- és elővesz egy flaska rumot. A fickó nagyot húz belőle, majd megszólal: </br>- Hál Istennek, megúsztuk egy karcolással! </br>- azzal átnyújtja a rumosflaskát a rabbinak. A rabbi fogja a flaskát, rácsavarja a kupakot és elrakja, ekkor a fickó megkérdi: </br>- Hát te nem iszol velem? Mire a rabbi: </br>- Én majd csak a helyszínelés meg a szondáztatás után..."
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+4, "Két rabló az ékszerbolt előtt néz a kirakatban egy különleges gyémánt nyakláncot. </br>- Szerinted mennyit kapnánk ezért a gyönyörű gyémánt nyakláncért? </br>- Hááát,  talán 5</br>-1 évet…"
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+4, "Mintha már láttam volna magát valahol! </br>- mondja gyanakodva a bíró a vádlottnak. </br>- Igen, uram! </br>- szól az reménykedve. </br>- Tavaly télen én adtam hegedűórákat a fiának. </br>- Ja persze, emlékszem. Húsz év!"
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+4, "A börtönlelkész kérdezi az elítélttől: </br>- Miért csukták le? </br>- A hitem miatt. </br>- ??? </br>- Azt hittem, nem derül ki amit tettem."
+);
+
+
+INSERT INTO jokes(category, full)
+VALUES
+(
+4, "Hogy haladsz a házépitéssel? </br>- kérdi az egyik tolvaj a másikat. </br>- Téglát már szereztem. Cementet, homokot és meszet is. De hogy kőműveseket honnan lopok?"
+);
+
